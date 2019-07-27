7648c90a31ef5fb9c17111a300ddd9b4a66bd6a6
jodillion <dillionjoann@gmail.com>
2019-07-27 12:11:12 -0400

    added factory bot spec for user

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "validations" do
+    subject { FactoryGirl.create(:user) }
+  end
+
+  it { should have_valid(:provider).when("Strava") }
+  it { should_not have_valid(:provider).when(nil, "")}
+
+  it { should have_valid(:uid).when("12345") }
+  it { should_not have_valid(:uid).when(nil, "")}
+
 end
