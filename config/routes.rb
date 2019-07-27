4a52f3f7c347e50f55309fe8d8a5192ff3e6e8f5
jodillion <dillionjoann@gmail.com>
2019-07-27 11:04:46 -0400

    added user controller and index page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'homes#index'
-  devise_for :users
   resources :homes, only: [:index]
+  resources :users, only: [:index]
 
   get '/auth/strava/callback', to: 'sessions#create'
   get 'logout', to: 'sessions#destroy'
