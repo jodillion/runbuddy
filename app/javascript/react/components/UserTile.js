69c7910d8d6bb57eb3f72b6451c44dc69c7dd923
jodillion <dillionjoann@gmail.com>
2019-07-30 13:56:14 -0400

    syntax error again

--- app/javascript/react/components/UserTile.js
+++ app/javascript/react/components/UserTile.js
@@ -6,7 +6,7 @@ const UserTile = props => {
   return()
     <div class="grid x">
       <div class="athlete-profile small-3 cell">
-        <img src=user.profile alt="profile picture" width="250" height="250"/>
+        <img src={user.profile} alt="profile picture" width="250" height="250"/>
         <p>{user.firstname} {user.lastname} - {user.sex}</p>
         <p>{user.city}, {user.state}</p>
       </div>
