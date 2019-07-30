5046b854e1328127949128bb487e18bcdf6e7271
jodillion <dillionjoann@gmail.com>
2019-07-30 14:09:43 -0400

    connected react files with controller and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
   root 'homes#index'
   resources :homes, only: [:index]
-  resources :users, only: [:index]
 
   get '/auth/strava/callback', to: 'sessions#create'
   get 'logout', to: 'sessions#destroy'
+
+  namespace :api do
+    namespace :v1 do
+      resources :users, only: [:index, :show]
+    end
+  end
 end
