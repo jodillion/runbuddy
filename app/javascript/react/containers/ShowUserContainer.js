f989f2db58bef5d5ab7200d31232fba280ad483a
jodillion <dillionjoann@gmail.com>
2019-07-31 23:51:07 -0400

    Added in more stats and some styling for show page

--- app/javascript/react/containers/ShowUserContainer.js
+++ app/javascript/react/containers/ShowUserContainer.js
@@ -24,6 +24,7 @@ class ShowUserContainer extends React.Component {
       }
     })
     .then((responseBody) => {
+      debugger
       return this.setState({
         recent_run_totals: responseBody.recent_run_totals,
         all_run_totals: responseBody.all_run_totals,
@@ -41,8 +42,18 @@ class ShowUserContainer extends React.Component {
         <StatTile
           id={this.state.ytd_run_totals.id}
           recent_distance={this.state.recent_run_totals.distance}
+          recent_achievement_count={this.state.recent_run_totals.achievement_count}
+          recent_count={this.state.recent_run_totals.count}
+          recent_time={this.state.recent_run_totals.elapsed_time}
+          recent_elevation={this.state.recent_run_totals.elevation_gain}
           all_distance={this.state.all_run_totals.distance}
+          all_count={this.state.all_run_totals.count}
+          all_time={this.state.all_run_totals.elapsed_time}
+          all_elevation={this.state.all_run_totals.elevation_gain}
           ytd_distance={this.state.ytd_run_totals.distance}
+          ytd_count={this.state.ytd_run_totals.count}
+          ytd_time={this.state.ytd_run_totals.elapsed_time}
+          ytd_elevation={this.state.ytd_run_totals.elevation_gain}
         />
       </div>
     )
