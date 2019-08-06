4d9c739b2ff6220f1eb97b9382dfeaabb02235ed
jodillion <dillionjoann@gmail.com>
2019-08-06 11:57:53 -0400

    restricted access to index page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,21 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user
+
+  protect_from_forgery with: :null_session
+  protect_from_forgery unless: -> { request.format.json }
 
   def index
   end
 
   def show
+  end
+
+  private
+
+  def authenticate_user
+    if !logged_in?
+      redirect_to "/auth/strava"
+    end
   end
 
 end
