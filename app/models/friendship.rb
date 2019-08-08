945c1de64208a38a61adfb06a099006624b2a4c0
jodillion <dillionjoann@gmail.com>
2019-08-08 09:47:50 -0400

    removed uniqueness constraints for friendships

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -1,6 +1,6 @@
 class Friendship < ApplicationRecord
-  validates :friend_id, uniqueness: true
-  validates :user_id, uniqueness: true
+  validates :friend_id
+  validates :user_id
   belongs_to :user
   belongs_to :friend, class_name: "User"
 
