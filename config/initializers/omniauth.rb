ad11596ca2eb1c9b43600ea8b9116edb5fe70d19
jodillion <dillionjoann@gmail.com>
2019-11-20 14:56:41 -0500

    changed scope from public to read

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :strava, ENV['STRAVA_CLIENT_ID'], ENV['STRAVA_SECRET_KEY'], scope: 'public'
+  provider :strava, ENV['STRAVA_CLIENT_ID'], ENV['STRAVA_SECRET_KEY'], scope: 'read'
 end
